7035f1261113ae7aac85397c6b62cc193267daeb
lexashvetsoff <lexa.shvetsoff@yandex.ru>
2021-05-31 13:42:29 +0800

    create: reviews section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const mySwiper = new Swiper('.hotel-slider', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -9,4 +9,15 @@ const mySwiper = new Swiper('.hotel-slider', {
   },
 
   effect: "coverflow",
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
 });
